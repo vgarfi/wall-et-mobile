082f036805e9e2a1d9082d49447108272d4993c7
Valentin <44127331+vgarfi@users.noreply.github.com>
2024-11-13 13:23:49 -0300

    feat: initial commit

---
+++ app/src/main/java/com/example/wall_etmobile/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.wall_etmobile
+
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Colors
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.example.wall_etmobile.ui.theme.WalletMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WalletMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        "HCI Wall-et",
+        modifier = modifier,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WalletMobileTheme {
+        Greeting("Android")
+    }
+}
