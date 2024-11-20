e2dafbde9553299b8bf12902ef5c244950d177a9
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2024-11-20 00:23:35 -0300

    fea: adaptative button

--- app/src/main/java/com/example/wall_etmobile/design_kit/shared/ActionButton.kt
+++ app/src/main/java/com/example/wall_etmobile/design_kit/shared/ActionButton.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -17,9 +19,15 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,7 +40,10 @@ import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.window.core.layout.WindowHeightSizeClass
+import androidx.window.core.layout.WindowWidthSizeClass
 import com.example.wall_etmobile.R
+
 @Composable
 fun ActionButton(
     title: String,
@@ -45,8 +56,11 @@ fun ActionButton(
     elevation: Boolean = false,
     enabled: Boolean = true,
 ) {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
     val containerColor = colorResource(id = containerColorRes)
     val contentColor = colorResource(id = contentColorRes)
+    var adaptativeHeight by remember { mutableDoubleStateOf(0.0)}
+    var adaptativeWidth by remember { mutableDoubleStateOf(0.0)}
     val buttonColors: ButtonColors = if (elevation) {
         ButtonDefaults.elevatedButtonColors(
             containerColor = containerColor,
@@ -66,20 +80,32 @@ fun ActionButton(
     val minWidth = textLayoutResult.size.width
     val minHeight = textLayoutResult.size.height
 
-    val configuration = LocalConfiguration.current
-    val adjustedHeight = if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-        height * 1.2
-    } else {
-        height.toDouble()
-    }.toInt()
+    when (windowSizeClass.windowWidthSizeClass) {
+        WindowWidthSizeClass.COMPACT -> {
+            adaptativeWidth = (width*1f).toDouble()
+        }
+        WindowWidthSizeClass.MEDIUM -> {
+            adaptativeWidth = (width*1.5f).toDouble()
+        }
+        WindowWidthSizeClass.EXPANDED -> {
+            adaptativeWidth = (width*2f).toDouble()
+        }
+    }
 
-    val adjustedWidth = if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-        width * 1.6
-    } else {
-        width.toDouble()
-    }.toInt()
+    when (windowSizeClass.windowHeightSizeClass) {
+        WindowHeightSizeClass.COMPACT -> {
+            adaptativeHeight = (height*1f).toDouble()
+        }
+        WindowHeightSizeClass.MEDIUM -> {
+            adaptativeHeight = (height*1.3f).toDouble()
+        }
+        WindowHeightSizeClass.EXPANDED -> {
+            adaptativeHeight = (height*2f).toDouble()
+        }
+    }
 
-    val adjustedFontSize = if (adjustedWidth > 300) 18.sp else if (adjustedWidth > 150) 16.sp else 14.sp
+
+    val adjustedFontSize = if (adaptativeWidth > 300) 18.sp else if (adaptativeWidth > 150) 16.sp else 14.sp
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -93,8 +119,8 @@ fun ActionButton(
                 modifier = modifier
                     .widthIn(min = minWidth.dp)
                     .heightIn(min = minHeight.dp)
-                    .height(adjustedHeight.dp)
-                    .width(adjustedWidth.dp)
+                    .height(adaptativeHeight.dp)
+                    .width(adaptativeWidth.dp)
                     ,
                 colors = buttonColors,
                 contentPadding = PaddingValues(horizontal = 0.dp, vertical = 0.dp),
@@ -115,8 +141,8 @@ fun ActionButton(
                 modifier = modifier
                     .widthIn(min = minWidth.dp)
                     .heightIn(min = minHeight.dp)
-                    .height(adjustedHeight.dp)
-                    .width(adjustedWidth.dp),
+                    .height(adaptativeHeight.dp)
+                    .width(adaptativeWidth.dp),
                 contentPadding = PaddingValues(horizontal = 0.dp, vertical = 0.dp),
                 colors = buttonColors,
                 enabled = enabled
@@ -134,15 +160,55 @@ fun ActionButton(
     }
 }
 
-@Preview(showBackground = true )
+
+@Preview(
+    name = "Small Screen",
+    device = "spec:width=320dp,height=640dp,dpi=320", // Custom small screen
+    showBackground = true,
+    showSystemUi = true
+)
+@Preview(
+    name = "Large Screen",
+    device = "spec:width=720dp,height=1280dp,dpi=320", // Custom large screen
+    showBackground = true,
+    showSystemUi = true
+)
+@Preview(
+    name = "Small Screen",
+    device = "spec:width=320dp,height=640dp,dpi=320",
+    showBackground = true,
+    showSystemUi = true
+)
+@Preview(
+    name = "Small Screen landscape",
+    device = "spec:height=320dp,width=640dp,dpi=320",
+    showBackground = true,
+    showSystemUi = true
+)
+@Preview(
+    name = "Small Screen",
+    device = "spec:width=1500dp,height=800dp,dpi=320", // Custom small screen
+    showBackground = true,
+    showSystemUi = true
+)
+@Preview(
+    name = "Small Screen landscape",
+    device = "spec:height=1500dp,width=800dp,dpi=320", // Custom small screen
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun ActionButtonPreviews() {
-    Column {
+    Column (
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         ActionButton(
             title = "Ingresar",
             onClick = { /* Do something */ },
             elevation = true,
-            enabled = false
+            enabled = true
         )
     }
 }
