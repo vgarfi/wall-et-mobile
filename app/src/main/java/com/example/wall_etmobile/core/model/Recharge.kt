6ad1def17df71c2b6eaf83da43e08ed43600e535
Valentin <44127331+vgarfi@users.noreply.github.com>
2024-11-22 16:17:14 -0300

    feat: removed recharge

--- app/src/main/java/com/example/wall_etmobile/core/model/Recharge.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wall_etmobile.core.model
-
-import java.time.temporal.TemporalAmount
-
-data class Recharge (
-    val amount: Long,
-    val id: String,
-    val dateTime: String,
-)
