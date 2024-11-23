0f7d0fd5c9a4413a498ff759c202b91e9486c6ff
Valentin <44127331+vgarfi@users.noreply.github.com>
2024-11-23 13:23:31 -0300

    feat: chahnged home header

--- app/src/main/java/com/example/wall_etmobile/core/designKit/TitleWithTextButton.kt
+++ app/src/main/java/com/example/wall_etmobile/core/designKit/TitleWithTextButton.kt
@@ -4,14 +4,25 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.wall_etmobile.core.theme.MainBlack
+import com.example.wall_etmobile.core.theme.MainPurple
+import compose.icons.FontAwesomeIcons
 
 @Composable
 fun TitleWithTextButton(
@@ -26,22 +37,31 @@ fun TitleWithTextButton(
     ) {
         Text(
             text = stringResource(title),
-            fontSize = MaterialTheme.typography.titleSmall.fontSize,
-            fontWeight = MaterialTheme.typography.titleSmall.fontWeight,
-            fontFamily = MaterialTheme.typography.titleSmall.fontFamily,
-            color = MaterialTheme.colorScheme.primary,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.W600,
+            color = MainBlack,
             modifier = Modifier.weight(1f)
         )
-        ElevatedButton(
+        TextButton(
             onClick = {
                 onClick()
             },
-            colors = ButtonDefaults.elevatedButtonColors(MaterialTheme.colorScheme.background)
         ) {
-            Text(
-                text = stringResource(textButton),
-                color = MaterialTheme.colorScheme.onBackground
-            )
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = stringResource(textButton),
+                    color = MainPurple
+                )
+                Icon(
+                    Icons.Default.KeyboardArrowRight,
+                    contentDescription = "view",
+                    tint = MainPurple,
+                    modifier = Modifier.size(18.dp)
+                )
+            }
         }
     }
 }
