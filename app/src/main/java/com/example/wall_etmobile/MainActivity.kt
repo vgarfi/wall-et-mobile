c6eed44ef8e745400eb6e4a8c2510bd758f606f1
Valentin <44127331+vgarfi@users.noreply.github.com>
2024-11-24 11:31:01 -0300

    feat: chaged app font

--- app/src/main/java/com/example/wall_etmobile/MainActivity.kt
+++ app/src/main/java/com/example/wall_etmobile/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 //        enableEdgeToEdge()
 //        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
         setContent {
             WalletMobileTheme {
